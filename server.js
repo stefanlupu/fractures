e90ab524b64f458244d2474812380207ebfd13ae
Stefan Lupu <>
2023-06-18 23:08:54 +0100

    Display hello world triangle on screen using shaders from the server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 
@@ -12,6 +13,20 @@ app.get('/', (req, res) => {
 
 app.get('/shader.js', (req, res) => {
     res.sendFile(path.join(__dirname, 'shader.js'));
+});
+
+app.get('/glsl/:shader_type', (req, res) => {
+    const shaderType = req.params.shader_type;
+    const filePath = `./${shaderType}.glsl`;
+
+    fs.readFile(filePath, 'utf-8', (err, data) => {
+	if (err) {
+	    console.error('Error reading file: ', err);
+	    return res.status(500).send('Internal Server Error');
+	}
+	console.log(`[x] sending shader: ${shaderType}..`)
+	res.send(data);
+    });
 });
 
 app.listen(port, host, () => {
--- shader.js
+++ shader.js
@@ -1,7 +1,89 @@
-var InitCanvas = function() {
+var main = async function() {
     console.log('shading..')
     var canvas = document.getElementById('shader-surface');
+    // these dimensions match the css which means all pixels should be in frame
+    canvas.width = 720;
+    canvas.height = 480;
     var gl = canvas.getContext('webgl');
-    gl.clearColor(1.0, 0.0, 0.0, 1.0);
+    
+    var vertexSource = await getShaderSource('vertex');
+    var fragmentSource = await getShaderSource('fragment');
+
+    var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexSource);
+    var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentSource);
+
+    var program = createProgram(gl, vertexShader, fragmentShader);
+
+    var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+    var positionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+    var positions = [
+	-0.5, -0.5,
+	0, 0.5,
+	0.5, -0.5,
+    ];
+
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+    gl.clearColor(0.9, 0.85, 0.8, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.useProgram(program);
+
+    gl.enableVertexAttribArray(positionAttributeLocation);
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    var size = 2;
+    var type = gl.FLOAT;
+    var normalize = false;
+    var stride = 0;
+    var offset = 0;
+
+    gl.vertexAttribPointer(
+	positionAttributeLocation, size, type, normalize, stride, offset
+    );
+
+    var primitiveType = gl.TRIANGLES;
+    var offset = 0;
+    var count = 3;
+
+    gl.drawArrays(primitiveType, offset, count);
 }();
+
+async function getShaderSource(type) {
+    var shaderSource = await fetch(`http://localhost:8080/glsl/${type}`)
+	.then(res => { return res.text() })
+	.then(data => { return data }); 
+
+    return shaderSource;
+}
+
+function createShader(gl, type, source) {
+    var shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+
+    var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+    if (success) {
+	return shader;
+    }
+
+    console.log(gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
+}
+
+function createProgram(gl, vertexShader, fragmentShader) {
+    var program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+
+    var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+    if (success) {
+	return program;
+    }
+    
+    console.log(gl.getProgramInfoLog(program));
+    gl.deleteProgram(program);
+}
